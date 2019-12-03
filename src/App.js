9ffead71430ad2a2f11211ebff4cff7ec41de27b
unknown <Administrateur@HEVinci_D4XXMT1.vinci.be>
2019-12-03 13:54:27 +0100

    test pile techno

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestPileTechno from './TestPileTechnoComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <TestPileTechno/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/TestPileTechnoComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class TestPileTechnoComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          msg : ""
+        };
+      }
+    componentDidMount() {
+        fetch("/myapp/myresource")
+          .then(res => res.text())
+          .then((result) => {
+            this.setState({
+                msg : "Yeeeees : " + result
+            });
+            },(error) => {
+            this.setState({
+                msg : "Noooooo : " + error
+            });
+            }
+        )
+    }
+    render() {
+        const { msg } = this.state;
+        return <div>{msg}</div>;
+    }
+  }
+
+export default TestPileTechnoComponent;
