528213e99dbc1256d71111a39dbd0dae6a85a339
Andy <andy.ieitz@student.vinci.be>
2019-12-06 09:19:43 +0100

    ajout d'un boutton pour appliquer la configuration

--- src/components/configuration/ConfigurationForm.jsx
+++ src/components/configuration/ConfigurationForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Form, FormGroup, FormCheck } from "react-bootstrap";
+import { Row, Col, Form, FormGroup, FormCheck, Button } from "react-bootstrap";
 
 const ConfigurationForm = ({
     malvoyant, //bool
@@ -13,21 +13,27 @@ const ConfigurationForm = ({
     return (
         <Row>
                 <Col>
-                    <Form.Group controlId = "formHorizontalMalvoyant">
-                        <Form.Check type = "checkbox" label = "L'enfant est malvoyant." /* OU Mode Malvoyant */ /> 
-                    </Form.Group>
+                    <Form /*onSubmit = {configure} */>
+                        <Form.Group controlId = "formHorizontalMalvoyant">
+                            <Form.Check type = "checkbox" label = "L'enfant est malvoyant." /* OU Mode Malvoyant */ /> 
+                        </Form.Group>
 
-                    <Form.Group controlId = "formHorizontalMouvement">
-                        <Form.Check type = "checkbox" label = "L'enfant présente des problème de mouvement." /*OU Mode difficulté de mouvement */ />
-                    </Form.Group>
+                        <Form.Group controlId = "formHorizontalMouvement">
+                            <Form.Check type = "checkbox" label = "L'enfant présente des problème de mouvement." /*OU Mode difficulté de mouvement */ />
+                        </Form.Group>
 
-                    <Form.Group controlId = "formHorizontalDaltonien">
-                        <Form.Check type = "checkbox" label = "L'enfant est daltonien" /* OU Mode Daltonien */ />
-                    </Form.Group>
+                        <Form.Group controlId = "formHorizontalDaltonien">
+                            <Form.Check type = "checkbox" label = "L'enfant est daltonien" /* OU Mode Daltonien */ />
+                        </Form.Group>
 
-                    <Form.Group controlId = "formHorizontalHelpFirst">
-                        <Form.Check type = "checkbox" label = "Je préfère commencer le jeu par besoins d'aide." />
-                    </Form.Group>
+                        <Form.Group controlId = "formHorizontalHelpFirst">
+                            <Form.Check type = "checkbox" label = "Je préfère commencer le jeu par besoins d'aide." />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Button type = "submit" /*onSubmit = {configure}*/ >Appliquer</Button>
+                        </Form.Group>
+                    </Form>
                 </Col>
         </Row>
     )
