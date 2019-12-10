3b957f28252a9b03dd837033cd760e907b9a1768
Andy <andy.ieitz@student.vinci.be>
2019-12-10 10:37:11 +0100

    merge pull

--- src/components/login/LoginContainer.jsx
+++ src/components/login/LoginContainer.jsx
@@ -4,7 +4,7 @@ import LoginForm from "./LoginForm";
 import './Login.css';
 
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
 
 import '../../index.css'
 
@@ -15,7 +15,7 @@ const LoginContainer = () => {
     const [nom, setNom] = useState("");
     const [code, setCode] = useState("");
 
-    const { isAuthenticated, setAuthentification } = useState("");
+    const [isAuthenticated, setAuthentification ] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
 
     // manage event fuction to update the state
@@ -27,29 +27,79 @@ const LoginContainer = () => {
         }
     }
 
-    //render
-    return (
+    //méthode d'authentification
+    const authentificate = async e => {
+        e.preventDefault();
 
-        <div>
-            <div>
-                <h1 class='Jeu-Titre Dyslexic'>HandicApp</h1>
-                <h2 class='Jeu-SousTitre Dyslexic'>Login</h2>
-            </div>
+        const user = {
+            nom : nom,
+            code : code
+        };
 
-            <div className = "blue-bg">
-                <LoginForm
-                    nom = {nom}
-                    code = {code}
-                    errorMessage = {errorMessage}
-                    //authentificate = {} Méthode répondant à l'évenement -> devra faire un fetch et ensuite un Redirect si le fetch retourne 200
-                    onInputChange = {onInputChange}
-                ></LoginForm>
-                <Link to="jeu">Une fois connecté si enfant</Link><br/><br/>
-                <Link to="animateur">Une fois connecté si animateur</Link><br/><br/>
-                <Link to="test">Test de la pile technologique</Link>
-            </div>
-        </div>
-    );
+        try{
+            const response = await fetch(" https://api-pfe-dev.herokuapp.com/user/connection", {
+                method : "POST",
+                body : JSON.stringify(user),
+                headers : {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            const json = await response.json();
+
+            if (json.sucess) { //selon code HTTP
+                setAuthentification(true);
+            }else{
+                setErrorMessage(json.error);
+            }
+        }
+        catch (err){
+            console.error("LoginContainer::Error", err);
+            setAuthentification(false);
+            setErrorMessage(err.toString());
+        }
+    };
+
+    /*TODO : 
+        - fix error when connecting SyntaxError :
+          Unexpected token < in JSON at position 0
+
+        - Error appel is GET local
+
+        - make the connexion work between the API and webapp for checking.
+    */
+
+        //render
+        if(!isAuthenticated){
+            return (
+
+                <div>
+                    <div>
+                        <h1 className='Jeu-Titre Dyslexic'>HandicApp</h1>
+                        <h2 className='Jeu-SousTitre Dyslexic'>Login</h2>
+                    </div>
+
+                    <div className = "blue-bg">
+                        <LoginForm
+                            nom = {nom}
+                            code = {code}
+                            errorMessage = {errorMessage}
+                            authentificate = {authentificate} //Méthode répondant à l'évenement -> devra faire un fetch et ensuite un Redirect si le fetch retourne 200
+                            onInputChange = {onInputChange}
+                        ></LoginForm>
+                        <Link to="jeu">Une fois connecté si enfant</Link><br/><br/>
+                        <Link to="animateur">Une fois connecté si animateur</Link><br/><br/>
+                        <Link to="test">Test de la pile technologique</Link>
+                    </div>
+                </div>
+            );
+        }else{
+            return (
+
+            <p>Connecté avec succès</p>
+
+            );
+        }
     }
 
     export default LoginContainer;
